9fef30f52e43dd0b8af065bd10067f337bb70036
Arthur Noel <arthur@ischium.net>
2024-11-11 13:41:49 +0000

    tweak CircularError edge repr

--- src/zerolib/exc.py
+++ src/zerolib/exc.py
@@ -5,6 +5,6 @@ class CircularError(Exception):
     def __init__(self, edges: list):
         edges = [(edge[0], edge[2]) for edge in edges] + [(edges[-1][1], None)]
         super().__init__(
-            " => ".join(f"{edge[1]}" for edge in edges),
+            " => ".join(str(edge[1]) for edge in edges),
             edges,
         )
